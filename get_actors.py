91e945dc8ebfe33781e76ef9168b642b8ac2f161
Mark Jackson-Brown <mark.jacksonbrown@gmail.com>
2020-08-23 00:31:54 -0400

    actually get movie ids

--- get_actors.py
+++ get_actors.py
@@ -1,13 +1,23 @@
 import json
 import requests
 import csv
-
+import urllib.parse as ul
+import sys
 
 def get_movie_id(title):
     """ Get a movie id from the title"""
     print("getting movie id for " + title)
-    movie_id = None
-    return movie_id
+    enc_title = ul.quote_plus(title)
+    print("encoded title as " + enc_title)
+    
+    url = 'https://api.themoviedb.org/3/search/movie?api_key=' + key + '&language=en-US&query=' + enc_title + '&page=1&include_adult=false'
+    response = requests.get(url)
+    responsedict = response.json()
+    if len(responsedict['results']) > 0:
+        print('got results')
+        print(f"{responsedict['results'][0]['original_title']} has ID: {responsedict['results'][0]['id']}")
+        return str(responsedict['results'][0]['id'])
+    return None
 
 def get_cast_from_id(movie_id):
     """ Get the cast of a movie in a dict from the movie id"""
@@ -26,17 +36,6 @@ f = open("key", "r")
 key = f.readline()
 key = key.strip()
 print(key)
-
-url = 'https://api.themoviedb.org/3/movie/550/credits?api_key=' + key
-url = url.strip()
-print(url)
-
-response = requests.get(url)
-
-print(response.text)
-
-
-
 
 # Read list of movies
 
@@ -59,5 +58,13 @@ with open('episodes.csv', mode='r') as csv_file:
 
 # for each movie, pull cast list
 # for each cast member, insert movie title and episode # into a list
+
+url = 'https://api.themoviedb.org/3/movie/550/credits?api_key=' + key
+url = url.strip()
+print(url)
+
+# response = requests.get(url)
+
+# print(response.text)
 
 
